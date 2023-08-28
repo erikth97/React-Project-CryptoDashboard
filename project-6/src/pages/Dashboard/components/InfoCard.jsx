f0c6d48ff6512d57f8e7902ae759e9b346710827
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-08-28 14:59:36 -0600

    infoCard

--- project-6/src/pages/Dashboard/components/InfoCard.jsx
+++ project-6/src/pages/Dashboard/components/InfoCard.jsx
@@ -1,12 +1,31 @@
-import { Tag } from '@chakra-ui/react'
-import { CustomCard } from '../../../chakra/CustomCard';
+import { Tag, Text } from "@chakra-ui/react";
+import { CustomCard } from "../../../chakra/CustomCard";
 
-const InfoCard = () => {
+const InfoCard = ({ imgUrl, text, tagText, inverted }) => {
   return (
-    <CustomCard>
-        <Tag bg="p.purple">Loan</Tag>
+    <CustomCard
+      bgColor={inverted ? "p.purple" : "white"}
+      bgImage={imgUrl}
+      bgSize="cover"
+      bgRepeat="no-repeat"
+    >
+      <Tag
+        color={inverted ? "p.purple" : "white"}
+        bg={inverted ? "white" : "p.purple"}
+        borderRadius="full"
+      >
+        {tagText}
+      </Tag>
+      <Text
+        mt="4"
+        fontWeight="medium"
+        textStyle="h5"
+        color={inverted ? "white" : "black.80"}
+      >
+        {text}
+      </Text>
     </CustomCard>
   );
 };
 
-export default InfoCard
+export default InfoCard;
