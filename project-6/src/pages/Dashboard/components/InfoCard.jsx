26182ee36f9246c301c166347afafa173121d721
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-08-28 13:05:20 -0600

    infoCard

---
+++ project-6/src/pages/Dashboard/components/InfoCard.jsx
@@ -0,0 +1,12 @@
+import { Tag } from '@chakra-ui/react'
+import { CustomCard } from '../../../chakra/CustomCard';
+
+const InfoCard = () => {
+  return (
+    <CustomCard>
+        <Tag bg="p.purple">Loan</Tag>
+    </CustomCard>
+  );
+};
+
+export default InfoCard
