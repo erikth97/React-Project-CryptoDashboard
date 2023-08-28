d1ebd2b404181e289a43b9a67794cb4796939a95
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-08-28 13:05:11 -0600

    dashboard

--- project-6/src/pages/Dashboard/Dashboard.jsx
+++ project-6/src/pages/Dashboard/Dashboard.jsx
@@ -2,6 +2,8 @@ import { Grid, GridItem } from "@chakra-ui/react";
 import DashboardLayout from "../../components/DashboardLayout";
 import PortfolioSection from "./components/PortfolioSection";
 import PriceSection from "./components/PriceSection";
+import Transactions from "./components/Transactions";
+import InfoCard from "./components/InfoCard";
 
 const Dashboard = ({}) => {
   return (
@@ -18,6 +20,12 @@ const Dashboard = ({}) => {
       </GridItem>
       <GridItem colSpan={1}>
         <PriceSection />
+      </GridItem>
+      <GridItem colSpan={1}>
+        <Transactions />
+      </GridItem>
+      <GridItem colSpan={1}>
+        <InfoCard />
       </GridItem>
       </Grid>
     </DashboardLayout>  
