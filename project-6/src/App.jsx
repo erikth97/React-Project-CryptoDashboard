44634fa77e6cfe8a07c072a78c3a9ab712c90c3a
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-08-29 12:29:55 -0600

    Appjsx

--- project-6/src/App.jsx
+++ project-6/src/App.jsx
@@ -1,11 +1,29 @@
-import Dashboard from "./pages/Dashboard/Dashboard"
+import Dashboard from "./pages/Dashboard/Dashboard";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import TransactionsPage from "./pages/Transaction/Transaction"
+import Support from "./pages/Support/Support";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+  {
+    path: "/transactions",
+    element: <TransactionsPage/>,
+  },
+  {
+    path: "/support",
+    element: <Support />,
+  },
+]);
 
 function App() {
   return (
-  <>
-    <Dashboard></Dashboard>  
-   </>
-  )
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
-export default App
+export default App;
