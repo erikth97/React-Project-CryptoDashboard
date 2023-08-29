d403a9c5b2de4a3c78a5a0543ad92a88b3ff9957
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-08-29 12:30:42 -0600

    transaction

--- project-6/src/pages/Transaction/Transaction.jsx
+++ project-6/src/pages/Transaction/Transaction.jsx
@@ -1,7 +1,89 @@
-const Transaction = () => {
-  return (
-    <div>Transaction</div>
-  )
-}
+import { 
+  Button, 
+  Card, 
+  Flex, 
+  HStack, 
+  Icon, 
+  Input, 
+  InputGroup, 
+  InputLeftElement, 
+  Tag} from "@chakra-ui/react";
+import DashboardLayout from "../../components/DashboardLayout";
+import { FaFileDownload } from "react-icons/fa";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import TransactionTable from "./components/TransactionTable";
+import { FcSearch } from "react-icons/fc";
+const TransactionPage = () => {
+  const tabs = [
+    {
+      name: "All",
+      count: 349,
+    },
+    {
+      name: "Deposit",
+      count: 114,
+    },
+    {
+      name: "Widthdraw",
+      count: 55,
+    },
+    {
+      name: "Trade",
+      count: 50,
+    },
+  ];
 
-export default Transaction
+  return (
+    <DashboardLayout title="Transactions">
+      <Flex justify="end" mt="6" mb="3">
+        <Button leftIcon={<Icon as={FaFileDownload} />}>Export CSV</Button>
+      </Flex>
+      <Card borderRadius="1rem">
+        <Tabs>
+          <TabList
+            pt="4"
+            display="flex"
+            w="full"
+            justifyContent="space-between"
+            bgGradient="linear(to-b, #dad7ee, white)"
+          >
+            <HStack>
+              {tabs.map((tab) => (
+                <Tab key={tab.name} display="flex" gap="2" pb="4">
+                  {tab.name}{" "}
+                  <Tag colorScheme="gray" borderRadius="full">
+                    {tab.count}
+                  </Tag>
+                </Tab>
+              ))}
+            </HStack>
+
+            <InputGroup maxW="200px" pr="6">
+              <InputLeftElement pointerEvents="none">
+                <Icon as={FcSearch} />
+              </InputLeftElement>
+              <Input type="tel" placeholder="Search..." />
+            </InputGroup>
+          </TabList>
+
+          <TabPanels>
+            <TabPanel>
+              <TransactionTable />
+            </TabPanel>
+            <TabPanel>
+              <TransactionTable />
+            </TabPanel>
+            <TabPanel>
+              <TransactionTable />
+            </TabPanel>
+            <TabPanel>
+              <TransactionTable />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Card>
+    </DashboardLayout>
+  );
+};
+
+export default TransactionPage;
